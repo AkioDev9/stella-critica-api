4b30d61d7663e909cb385b6948baca0a8661a87b
AkioDev9 <akio.oshiro.dev@gmail.com>
2024-12-17 12:14:12 -0500

    Subida del repositorio

---
+++ src/main/java/com/example/stellacriticaapi/StellaCriticaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.stellacriticaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellaCriticaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StellaCriticaApiApplication.class, args);
+    }
+
+}
